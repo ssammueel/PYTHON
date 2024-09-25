8de40f9f765b027d23a85f7c839d56deb25ee313
Samuel <signfforce@gmail.com>
2024-09-25 11:58:07 +0300

    python

---
+++ INTRO/more/class/variables.py
@@ -0,0 +1,15 @@
+# variables are a containers for storing value
+a = 2;
+b = 3
+
+x  = a + b
+
+print(x)
+
+#Rules for naming variables
+# 1. cannot start with a number
+# 2. must start with a letter or an undersore character
+# 3. can only contain alpha numeric characters and underscore
+
+_3name = "samuel"
+
